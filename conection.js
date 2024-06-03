b0639356036a95ec0be964b25eae0b140181cb0d
CharlyLnk <carlosrincon816@gmail.com>
2024-06-03 16:56:24 -0600

    first commit

---
+++ conection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const config = {
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '1234',
+    database: 'agenda'
+}
+
+const connection = mysql.createConnection(config);
+
+connection.connect(function(err){
+    if (err) throw (err);
+    console.log('Conexión exitosa!!')
+});
+
+module.exports = connection;
