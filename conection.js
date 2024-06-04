d40693140e50cb99f4276539c2f3bb585d2d072b
SalvadorAR98 <146896834+SalvadorAR98@users.noreply.github.com>
2024-06-04 00:04:08 -0600

    nav bar en todo
    nav bar en todo

--- conection.js
+++ conection.js
@@ -4,7 +4,10 @@ const config = {
     host: 'localhost',
     port: 3306,
     user: 'root',
+<<<<<<< Updated upstream
     password: 'root',
+=======
+>>>>>>> Stashed changes
     database: 'agenda'
 }
 
